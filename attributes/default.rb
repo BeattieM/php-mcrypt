c55b068ab868f797704326c2f176692357a0a998
Michael Beattie <beattie@cloudspace.com>
2014-01-10 16:40:28 -0500

    Modified to install mcrypt on Ubuntu envs

--- attributes/default.rb
+++
@@ -1,7 +0,0 @@
-package "php-mcrypt" do
-	action [:install]
-end
-
-service "php-mcrypt" do
-	action [:enable,:start]
-end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,15 @@
-#
-# Cookbook Name:: php-mcrypt
-# Recipe:: default
-#
-# Copyright 2014, Example Com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+pkgs = value_for_platform(
+  [ "ubuntu" ] => {
+    "default" => %w{ php5-mcrypt }
+  }
+)
+
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+template "#{node['php']['ext_conf_dir']}/mcrypt.ini" do
+  mode "0644"
+end
